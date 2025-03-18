1aa2ffadaea0f3f647d52c562431fe10131cf29b
primeiro teste <felipekof01@gmail.com>
2025-03-17 22:47:07 -0300

    Meu primeiro programa em Python

--- primeiro programa.py
+++ primeiro programa.py
@@ -1,1 +1,25 @@
 print("Hello word!")
+
+#Exercício 1
+print("Felipe Lins\n")
+
+#Exercício 2
+a = 3
+b = 2
+print(2*a*3*b)
+print()
+
+#Exercício 3
+#print()
+#num1 = 2
+#num2 = 3
+#num3 = 4
+#soma = num1+num2+num3
+#print(soma)
+
+num1 = float(input("Número 1 = "))
+num2 = float(input("Número 2 = "))
+num3 = float(input("Número 3 = "))
+soma = num1+num2+num3
+print(soma)
+
